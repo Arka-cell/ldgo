06cd239b71024a85e0d9b2c208e22522eb7bf70b
Samir Ahmane <55358999+Arka-cell@users.noreply.github.com>
2022-07-21 15:18:09 +0100

    feat: handling database connection on Server run

--- api/server.go
+++ api/server.go
@@ -1,10 +1,33 @@
 package api
 
-import "github.com/Arka-cell/ldgo/api/controllers"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/Arka-cell/ldgo/api/controllers"
+	"github.com/joho/godotenv"
+)
 
 var server = controllers.Server{}
 
 func Run() {
+	var err error
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error getting env, not comming through %v", err)
+	} else {
+		fmt.Println("We are getting the env values")
+	}
+	println(os.Getenv("DB_USER") + " is now connected" + " to " + os.Getenv("DB_NAME"))
+	server.Migrate(
+		os.Getenv("DB_DRIVER"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_NAME"),
+	)
 	router := server.InitializeRouter()
 	router.Run("localhost:8080")
 }
