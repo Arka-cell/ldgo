d8afecf20a638e22d201292519bfa96c00971b60
Samir Ahmane <55358999+Arka-cell@users.noreply.github.com>
2022-07-21 15:17:27 +0100

    feat: created Shop and Product models

---
+++ api/models/Product.go
@@ -0,0 +1,107 @@
+package models
+
+import (
+	"errors"
+	"html"
+	"strings"
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
+
+type Product struct {
+	ID          uint32    `gorm:"primary_key;auto_increment" json:"id"`
+	Name        string    `gorm:"size:100;not null;unique" json:"name"`
+	Description string    `gorm:"size:512" json:"description"`
+	Shop        Shop      `json:"shop"`
+	ShopID      uint32    `gorm:"not null" json:"shop_id"`
+	CreatedAt   time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt   time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+}
+
+func (p *Product) Prepare() {
+	p.ID = 0
+	p.Name = html.EscapeString(strings.TrimSpace(p.Name))
+	p.Description = html.EscapeString(strings.TrimSpace(p.Description))
+	p.Shop = Shop{}
+	p.CreatedAt = time.Now()
+	p.UpdatedAt = time.Now()
+}
+
+func (p *Product) SaveProduct(db *gorm.DB) (*Product, error) {
+	var err error
+	err = db.Debug().Model(&Product{}).Create(&p).Error
+	if err != nil {
+		return &Product{}, err
+	}
+	if p.ID != 0 {
+		err = db.Debug().Model(&Shop{}).Where("id = ?", p.ShopID).Take(&p.Shop).Error
+		if err != nil {
+			return &Product{}, err
+		}
+	}
+	return p, nil
+}
+
+func (p *Product) FindAllProducts(db *gorm.DB) (*[]Product, error) {
+	var err error
+	products := []Product{}
+	err = db.Debug().Model(&Product{}).Limit(100).Find(&products).Error
+	if err != nil {
+		return &[]Product{}, err
+	}
+	if len(products) > 0 {
+		for i, _ := range products {
+			err := db.Debug().Model(&Shop{}).Where("id = ?", products[i].ShopID).Take(&products[i].Shop).Error
+			if err != nil {
+				return &[]Product{}, err
+			}
+		}
+	}
+	return &products, nil
+}
+
+func (p *Product) FindProductByID(db *gorm.DB, pid uint64) (*Product, error) {
+	var err error
+	err = db.Debug().Model(&Product{}).Where("id = ?", pid).Take(&p).Error
+	if err != nil {
+		return &Product{}, err
+	}
+	if p.ID != 0 {
+		err = db.Debug().Model(&Shop{}).Where("id = ?", p.ShopID).Take(&p.Shop).Error
+		if err != nil {
+			return &Product{}, err
+		}
+	}
+	return p, nil
+}
+
+func (p *Product) UpdateAProduct(db *gorm.DB) (*Product, error) {
+
+	var err error
+
+	err = db.Debug().Model(&Product{}).Where("id = ?", p.ID).Updates(Product{Name: p.Name, Description: p.Description, UpdatedAt: time.Now()}).Error
+	if err != nil {
+		return &Product{}, err
+	}
+	if p.ID != 0 {
+		err = db.Debug().Model(&Shop{}).Where("id = ?", p.ShopID).Take(&p.Shop).Error
+		if err != nil {
+			return &Product{}, err
+		}
+	}
+	return p, nil
+}
+
+func (p *Product) DeleteAProduct(db *gorm.DB, pid uint64, uid uint32) (int64, error) {
+
+	db = db.Debug().Model(&Product{}).Where("id = ? and Shop_id = ?", pid, uid).Take(&Product{}).Delete(&Product{})
+
+	if db.Error != nil {
+		if gorm.IsRecordNotFoundError(db.Error) {
+			return 0, errors.New("Product not found")
+		}
+		return 0, db.Error
+	}
+	return db.RowsAffected, nil
+}
---
+++ api/models/Shop.go
@@ -0,0 +1,156 @@
+package models
+
+import (
+	"log"
+	"time"
+
+	"errors"
+	"html"
+	"strings"
+
+	"github.com/badoux/checkmail"
+	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type Shop struct {
+	ID        uint32    `gorm:"primary_key;auto_increment" json:"id"`
+	Title     string    `gorm:"size:255;not null;unique" json:"nickname"`
+	Email     string    `gorm:"size:100;not null;unique" json:"email"`
+	Password  string    `gorm:"size:100;not null;" json:"password"`
+	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+}
+
+func Hash(password string) ([]byte, error) {
+	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+}
+
+func VerifyPassword(hashedPassword, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
+
+func (s *Shop) BeforeSave() error {
+	hashedPassword, err := Hash(s.Password)
+	if err != nil {
+		return err
+	}
+	s.Password = string(hashedPassword)
+	return nil
+}
+
+func (s *Shop) Prepare() {
+	s.ID = 0
+	s.Title = html.EscapeString(strings.TrimSpace(s.Title))
+	s.Email = html.EscapeString(strings.TrimSpace(s.Email))
+	s.CreatedAt = time.Now()
+	s.UpdatedAt = time.Now()
+}
+
+func (s *Shop) Validate(action string) error {
+	switch strings.ToLower(action) {
+	case "update":
+
+		if err := checkmail.ValidateFormat(s.Email); err != nil && s.Email != "" {
+			return errors.New("Invalid Email")
+		}
+
+		return nil
+	case "login":
+		if s.Password == "" {
+			return errors.New("Required Password")
+		}
+		if s.Email == "" {
+			return errors.New("Required Email")
+		}
+		if err := checkmail.ValidateFormat(s.Email); err != nil {
+			return errors.New("Invalid Email")
+		}
+		return nil
+
+	case "create":
+		if s.Title == "" {
+			return errors.New("Required Nickname")
+		}
+		if s.Password == "" {
+			return errors.New("Required Password")
+		}
+		if s.Email == "" {
+			return errors.New("Required Email")
+		}
+		if err := checkmail.ValidateFormat(s.Email); err != nil {
+			return errors.New("Invalid Email")
+		}
+		return nil
+	default:
+		return nil
+	}
+}
+
+func (s *Shop) SaveShop(db *gorm.DB) (*Shop, error) {
+
+	var err error
+	err = db.Debug().Create(&s).Error
+	if err != nil {
+		return &Shop{}, err
+	}
+	return s, nil
+}
+
+func (u *Shop) FindAllShops(db *gorm.DB) (*[]Shop, error) {
+	var err error
+	shops := []Shop{}
+	err = db.Debug().Model(&Shop{}).Limit(100).Find(&shops).Error
+	if err != nil {
+		return &[]Shop{}, err
+	}
+	return &shops, err
+}
+
+func (s *Shop) FindShopByID(db *gorm.DB, uid uint32) (*Shop, error) {
+	var err error
+	err = db.Debug().Model(Shop{}).Where("id = ?", uid).Take(&s).Error
+	if err != nil {
+		return &Shop{}, err
+	}
+	if gorm.IsRecordNotFoundError(err) {
+		return &Shop{}, errors.New("Shop Not Found")
+	}
+	return s, err
+}
+
+func (s *Shop) UpdateShop(db *gorm.DB, uid uint32) (*Shop, error) {
+
+	// To hash the password
+	err := s.BeforeSave()
+	if err != nil {
+		log.Fatal(err)
+	}
+	db = db.Debug().Model(&Shop{}).Where("id = ?", uid).Take(&Shop{}).UpdateColumns(
+		map[string]interface{}{
+			"password":  s.Password,
+			"title":     s.Title,
+			"email":     s.Email,
+			"update_at": time.Now(),
+		},
+	)
+	if db.Error != nil {
+		return &Shop{}, db.Error
+	}
+	// This is the display the updated user
+	err = db.Debug().Model(&Shop{}).Where("id = ?", uid).Take(&s).Error
+	if err != nil {
+		return &Shop{}, err
+	}
+	return s, nil
+}
+
+func (s *Shop) DeleteShop(db *gorm.DB, uid uint32) (int64, error) {
+
+	db = db.Debug().Model(&Shop{}).Where("id = ?", uid).Take(&Shop{}).Delete(&Shop{})
+
+	if db.Error != nil {
+		return 0, db.Error
+	}
+	return db.RowsAffected, nil
+}
