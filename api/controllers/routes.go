85ed6eff665e06b94da8c8fd655ef13c5126dfb2
Samir Ahmane <55358999+Arka-cell@users.noreply.github.com>
2022-07-22 10:42:51 +0100

    feat: added route to login

--- api/controllers/routes.go
+++ api/controllers/routes.go
@@ -8,5 +8,6 @@ func (s *Server) InitializeRouter() *gin.Engine {
 	router := s.Router()
 	router.GET("/", s.getHome)
 	router.POST("/api/v0/signup", s.signUp)
+	router.POST("/api/v0/login", s.login)
 	return router
 }
