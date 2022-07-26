584ee5b15701116a1a6273959afee2f323a1136d
Arka-cell <samirahmane.trading@gmail.com>
2022-07-26 16:50:31 +0100

    feat: created partialUpdateProduct controller

--- api/controllers/product_controllers.go
+++ api/controllers/product_controllers.go
@@ -52,3 +52,33 @@ func (s *Server) getProduct(c *gin.Context) {
 	product.FindProductByID(s.DB, uid)
 	c.IndentedJSON(http.StatusOK, gin.H{"product": product})
 }
+
+func (s *Server) partialUpdateProduct(c *gin.Context) {
+	uid, err := auth.ExtractTokenID(c.Request)
+	if err != nil {
+		c.IndentedJSON(http.StatusUnauthorized, gin.H{"message": "Unauthorized"})
+		return
+	}
+	pk, err := strconv.Atoi(c.Param("id"))
+	pk32 := uint64(pk)
+	if err != nil {
+		return
+	}
+	product := models.Product{}
+	product.FindProductByID(s.DB, pk32)
+	if product.ShopID != uid {
+		c.IndentedJSON(http.StatusUnauthorized, gin.H{"message": "Unauthorized"})
+		return
+	}
+	if err := c.BindJSON(&product); err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "Unable to bind JSON to credentials struct"})
+		return
+	}
+	pk64 := int32(pk32)
+	updatedProduct, err := product.PartialUpdateProduct(s.DB, pk64, product.Name, product.Description, product.Price)
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "Internal Server Error"})
+		return
+	}
+	c.IndentedJSON(http.StatusOK, gin.H{"product": updatedProduct})
+}
