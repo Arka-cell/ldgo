05dc1641d3b77a63df6d50e5fed544f2a79b252b
Arka-cell <samirahmane.trading@gmail.com>
2022-08-15 20:55:29 +0100

    feat: added route to delete products

--- api/controllers/routes.go
+++ api/controllers/routes.go
@@ -10,12 +10,13 @@ func (s *Server) InitializeRouter() *gin.Engine {
 	router.POST("/api/v0/signup", s.signUp)
 	router.POST("/api/v0/login", s.login)
 	router.PATCH("/api/v0/shops", s.partialUpdateShop)
-	router.POST("/api/v0/products", s.createProduct)
 	router.DELETE("/api/v0/shops", s.deleteShop)
 	router.GET("/api/v0/shops", s.getAllShops)
 	router.GET("/api/v0/shops/:id", s.getShop)
+	router.POST("/api/v0/products", s.createProduct)
 	router.GET("/api/v0/products", s.getAllProducts)
 	router.GET("/api/v0/products/:id", s.getProduct)
 	router.PATCH("/api/v0/products/:id", s.partialUpdateProduct)
+	router.DELETE("/api/v0/products/:id", s.deleteProduct)
 	return router
 }
